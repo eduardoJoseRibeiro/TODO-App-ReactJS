91cb4b6492dd9dcab6611b2603170b3f3011e0a6
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2017-12-11 17:20:11 -0200

    Adicionando o header

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <h1><PageHeader name="Sobre"  small="Nós"/></h1>
+        <h2>Nossa História</h2>
+        <p>Qualquer texto aqui</p>
+        <h2>Missão e visão</h2>
+        <p>Mas um qualquer texto aqui</p>
+        <h2>Imprensa</h2>
+        <p>Mais uma vez mais texto aqui</p>
     </div>
 )
