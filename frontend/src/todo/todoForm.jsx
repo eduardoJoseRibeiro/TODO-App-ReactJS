85ae447ba7e99cde0398a4b4267743cb0578afb2
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2017-12-12 11:00:44 -0200

    Adicionando o HandleChange

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,7 +5,11 @@ import IconButton from "../template/iconButton"
 export default props => (
     <div role="form" className="todoForm">
         <Grid cols="12 9 10">
-            <input type="text" id="description" className="form-control" placeholder="Adicione uma Tarefa"/>
+            <input type="text" id="description" 
+                className="form-control" 
+                placeholder="Adicione uma Tarefa"
+                onChange = {props.handleChange} 
+                value={props.description}/>
         </Grid>
         <Grid cols="12 3 2">
             <IconButton style="primary" icon="plus" onClick={props.handleAdd}></IconButton>    
